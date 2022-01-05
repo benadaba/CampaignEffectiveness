12a075ace3eb51894e7f5336b4582f714cb0bc37
Bernard Adabankah <adabankah.com@gmail.com>
2022-01-05 23:47:25 +0000

    installing cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Measuring Campaign Effectiveness with Causal Inference',
+    author='Bernard Adabankah',
+    license='MIT',
+)
